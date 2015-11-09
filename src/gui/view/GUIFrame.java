0642ae6610b80e0459295ef55fcf0fb9ce4577fa
Emily Orton <eort3611@740s-pl-106.local>
2015-11-09 13:23:21 -0700

    Finished GUIPanel

--- src/gui/view/GUIFrame.java
+++ src/gui/view/GUIFrame.java
@@ -2,19 +2,25 @@ package gui.view;
 
 import javax.swing.JFrame;
 import gui.controller.GUIController;
+import javax.swing.JPanel;
 
 public class GUIFrame extends JFrame
 {
 	private GUIController baseController;
+	private GUIPanel basePanel;
 	public GUIFrame(GUIController baseController)
 	{
 		this.baseController = baseController;
+		basePanel = new GUIPanel(baseController);
 		setFrame();
 
 	}
 	
 	private void setFrame()
 	{
+		this.setContentPane(basePanel);
+		this.setTitle("StupidWindowApp");
+		this.setResizable(false);
 		this.setSize(400,400);
 		this.setVisible(true);
 	}
--- src/gui/view/GUIPanel.java
+++ src/gui/view/GUIPanel.java
@@ -1,14 +1,37 @@
 package gui.view;
 import javax.swing.JPanel;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.SpringLayout;
 import gui.controller.GUIController;
 
 public class GUIPanel extends JPanel
 {
 	 private GUIController baseController;
-	 
+	 private JButton firstButton;
+	 private JTextField firstTextField;
+	 private SpringLayout baseLayout;
 	 public GUIPanel(GUIController baseController)
 	 {
-		 this.baseContolller = baseController;
+		 this.baseController = baseController;
+		 firstButton = new JButton("Do Not Click The Button");
+		 firstTextField = new JTextField("You can Type words here");
+		 baseLayout = new SpringLayout();
+		 
+		 setupPanel();
+		 setupLayout();
+		 setupListeners();
+	 }
+	 private void setupPanel()
+	 {
+		this.setLayout(baseLayout);
+		 this.add(firstButton);
+		this.add(firstTextField);
+	 }
+	 private void setupLayout(){
+		 
+	 }
+	 private void setupListeners(){
 		 
 	 }
 }
