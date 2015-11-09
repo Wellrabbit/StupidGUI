651195e26df71ce14c6eba62c3a23637bfde70bb
Emily Orton <eort3611@740s-pl-106.local>
2015-11-09 12:18:54 -0700

    GUIFrame intialized

---
+++ src/gui/controller/GUIController.java
@@ -0,0 +1,24 @@
+package gui.controller;
+
+import gui.view.GUIFrame;
+
+
+public class GUIController
+{ 
+	/**
+	 * Refrence to GUIFrame object in View.
+	 */
+	private GUIFrame baseFrame;
+	
+	public GUIController()
+	{
+		baseFrame = new GUIFrame(this);
+	}
+	
+	
+	public void start()
+	{
+		
+	}
+
+}
